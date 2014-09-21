6f4f8e32d4c6facba32070efd73f27059e46a880
ricknussell31 <ricknussell31@gmail.com>
2014-09-21 13:51:06 -0400

    Last 3 queries committed
    7. List	all	data	for	orders	in	January	or	April.
    8. List	all	data	for	orders	in	February	more	than	US$200.
    9. List	all	orders	from	the	customer	whose	cid	is	C005.

--- Lab 3.sql
+++ Lab 3.sql
@@ -20,3 +20,15 @@ WHERE (city != 'New York' AND city != 'London');
 select *
 FROM products
 WHERE (city != 'Dallas' AND city != 'Duluth' AND priceUSD <= 1.00);
+
+select *
+FROM orders
+WHERE (mon = 'jan' OR mon = 'apr');
+
+select * 
+FROM orders
+WHERE (mon = 'feb' AND dollars > 200.00);
+
+select *
+FROM orders
+WHERE cid = 'c005';
