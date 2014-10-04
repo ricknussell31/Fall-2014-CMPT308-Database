b3037cc963480620db2e90fad4e6711e6f5ffbff
ricknussell31 <ricknussell31@gmail.com>
2014-10-04 14:45:29 -0400

    Lab 4 first commit
    Nicholas Russell
    First question completed

---
+++ Lab 4- SQL Queries - The Sequel .sql
@@ -0,0 +1,17 @@
+﻿--Nicholas Russell--
+--Professor Labouseur--
+--October 4, 2014--
+--Lab 4: SQL Queries--
+
+--1. Get the cities of agents booking an orer for customer "TipTop".--
+select city
+from agents
+where aid in (select aid 
+	      from orders
+	      where cid in (select cid
+			    from customers
+	                    where name = 'Tiptop')
+	      )
+
+
+
