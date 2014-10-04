7e4e2539ea10bb050149ecea85e9fa035a978170
ricknussell31 <ricknussell31@gmail.com>
2014-10-04 14:53:40 -0400

    Fix 1 and added Q2
    Made Q1 more specific and Q2 finalized

--- Lab 4- SQL Queries - The Sequel .sql
+++ Lab 4- SQL Queries - The Sequel .sql
@@ -4,7 +4,7 @@
 --Lab 4: SQL Queries--
 
 --1. Get the cities of agents booking an orer for customer "TipTop".--
-select city
+select distinct city
 from agents
 where aid in (select aid 
 	      from orders
@@ -12,6 +12,20 @@ where aid in (select aid
 			    from customers
 	                    where name = 'Tiptop')
 	      )
+
+--2. Get the pids of products ordered through any agent who makes at least one order for a customer in Kyoto.--
+
+select distinct pid
+from orders
+where aid in (select aid
+	      from orders
+	      where cid in (select cid
+		            from customers
+		            where city = 'Kyoto')
+		            )
+
+		            
+--3. Find the cids and names of customers who never placed an order through agent a04.--
 
 
 
