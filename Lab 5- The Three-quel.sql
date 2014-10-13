d67e365b76e9a51349017bd439b25beb0766ac3e
ricknussell31 <ricknussell31@gmail.com>
2014-10-13 01:12:42 -0400

    Rest of document completed
    Queries completed, will come back to check one more time and to clean
    up unnecessary queries.

--- Lab 5- The Three-quel.sql
+++ Lab 5- The Three-quel.sql
@@ -1,6 +1,7 @@
 ﻿--Nicholas Russell--
 --Professor Labouseur--
 --Database Systems: Lab 5: The Three-quel--
+--October 13, 2014
 
 --1. Get the cities of agents booking an order for customer "TipTop". This time use joins; no sub-queries.--
 
@@ -8,17 +9,17 @@ select distinct a.city
 from agents a, orders o, customers c
 where a.aid = o.aid 
   and c.cid = o.cid
-  and c.name = 'Tiptop'
+  and c.name = 'Tiptop';
 
 --2. Get the ids of products ordered through any agent who makes at least one order for a customer in Kyoto. Use joins this time; no subqueries.
 
-select p.pid
-from products p, orders o, agents a, customers c 
-where o.pid = p.pid and
-      o.aid = a.aid and
-      o.cid = c.cid and
-      c.cid
-
+select distinct o2.pid
+from orders o1, customers c, agents a, orders o2
+where o1.aid = a.aid
+  and o1.cid = c.cid
+  and o2.aid = a.aid
+  and c.city = 'Kyoto';
+      
 
 --3. Get the names of customers who have never placed an order. Use a subquery. 
 
@@ -32,15 +33,37 @@ where cid in (select cid
 
 --4. Get the names of customers who have never placed an order. Use an outer join. 
 
-select *
+select c.name
 from customers c left outer join orders o
 on c.cid = o.cid
-
+where o.ordno is null;
 
 --5. Get the names of customers who placed at least one order through an agent in their city, along with those agent(s)' names. --
 
-
+select distinct c.name, a.name
+from orders o
+     inner join 
+     customers c 
+	on o.cid = c.cid
+     inner join 
+     agents a
+        on o.aid = a.aid
+where c.city = a.city;
 
 --6. Get the names of customers and agents in the same city, along with the name of the city, regardless of whether or not the customer has every placed an order with that agent. --
 
---7. Get the name and city of customers who line in THE city that makes the FEWEST different kinds of products.--
+select distinct c.name, a.name, c.city
+from customers c, orders o, agents a
+where c.cid = o.cid and
+      c.city = a.city;
+
+--7. Get the name and city of customers who live in THE city that makes the FEWEST different kinds of products.--
+
+select name, city
+from customers
+where city = (select city
+	      from (select sum(quantity) as "sum", city
+		    from products p
+		    group by city
+		    order by sum ASC
+		    limit 1) as "TheFewest");
