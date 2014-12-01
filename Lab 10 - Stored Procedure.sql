91f671440890c774209242adc339ae41681b1e61
ricknussell31 <ricknussell31@gmail.com>
2014-11-30 20:58:26 -0500

    Lab 10 Finished
    Stored Procedures lab done

---
+++ Lab 10 - Stored Procedure.sql
@@ -0,0 +1,45 @@
+﻿--Nicholas Russell--
+--Professor Labouseur--
+--Database Systems Management--
+--November 30, 2014--
+--Lab 10: Stored Procedure--
+
+--1. Write a function PreReqsFor(courseNum) where it returns the immediate prerequisites for the passed-in course number. --
+
+create or replace function PreReqsFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   course      int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select prereqnum
+      from prerequisites
+      where courseNum = course;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select PreReqsFor(308, 'results');
+Fetch all from results;
+
+--2. Write a function IsReReqFor(courseNum) which returns the courses for which the passed-in course number is an immediate pre-requisite. 
+
+create or replace function IsPreReqFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   prereq      int       := $1;
+   resultset   REFCURSOR := $2;
+begin
+   open resultset for 
+      select coursenum
+      from prerequisites
+      where prereq =  prereqnum;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select IsPreReqFor(220, 'results');
+Fetch all from results;
